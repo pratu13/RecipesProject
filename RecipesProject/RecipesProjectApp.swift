b55464232789c3335e9bb335043229712d281bc3
pratu13 <pratu0906@gmail.com>
2025-03-11 19:33:58 -0400

    Initial Commit

---
+++ RecipesProject/RecipesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesProjectApp.swift
+//  RecipesProject
+//
+//  Created by Pratyush on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipesProjectTests/RecipesProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesProjectTests.swift
+//  RecipesProjectTests
+//
+//  Created by Pratyush on 3/11/25.
+//
+
+import Testing
+@testable import RecipesProject
+
+struct RecipesProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
